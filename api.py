eb6e010e1dd3783ccadac3f3b09a43072d1c029d
Chhavi-16 <vaishuu1608@gmail.com>
2026-09-06 01:06:12 +0530

    Update app.run to use environment PORT variable

--- api.py
+++ api.py
@@ -47,4 +47,6 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)  # ✅ IMPORTANT
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)  # ✅ IMPORTANT
